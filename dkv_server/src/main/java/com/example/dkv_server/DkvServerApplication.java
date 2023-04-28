861d3a96016c04f1110d788310674cc443eef0c3
vinayak <vinayak@Ubuntu.myguest.virtualbox.org>
2023-04-29 01:00:12 +0530

    Creating template

---
+++ dkv_server/src/main/java/com/example/dkv_server/DkvServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dkv_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkvServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkvServerApplication.class, args);
+	}
+
+}
